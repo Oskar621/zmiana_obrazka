81d1de18e1e5b749a73387715ae95e2eddb4bf5a
Oskar621 <o_swiderski@o2.pl>
2021-11-24 07:55:11 +0100

    zrobione

---
+++ app/src/main/java/com/example/a24112021/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a24112021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_image1).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.amongusamumu)
+        }
+        findViewById<Button>(R.id.button_image2).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.oooo)
+        }
+        findViewById<Button>(R.id.button_image3).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.vislaud)
+        }
+        findViewById<Button>(R.id.button_image4).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pogchamp)
+        }
+    }
+
+
+}
